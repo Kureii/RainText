d4583f60f9585a89dd8c9a9b5536169013074fa3
Tomas Adamek (Kureii) <ad.tomik@seznam.cz>
2024-01-23 00:55:07 +0100

    connect SettingLoader, add "appColors", "appColorModes" and "appCurrentColorMode" from SettingLoader

--- src/main.cpp
+++ src/main.cpp
@@ -157,6 +157,7 @@ int main() {
 #include "RainText/gui/login_register_manager.h"
 #include "RainText/gui/generate_password.h"
 #include "RainText/gui/copy_to_clipboard.h"
+#include "RainText/settings/setting_loader.h"
 
 int main(int argc, char* argv[]) {
   QGuiApplication app(argc, argv);
@@ -165,9 +166,15 @@ int main(int argc, char* argv[]) {
   rain_text::gui::LoginRegisterManager login_register_manager;
   rain_text::gui::GeneratePassword generate_password;
   rain_text::gui::CopyToClipboard copy_to_clipboard;
+  rain_text::settings::SettingLoader settings_manager;
+
   engine.rootContext()->setContextProperty("login_register_manager", &login_register_manager);
   engine.rootContext()->setContextProperty("generate_password", &generate_password);
   engine.rootContext()->setContextProperty("copy_to_clipboard", &copy_to_clipboard);
+  engine.rootContext()->setContextProperty("settings_manager", &settings_manager);
+  engine.rootContext()->setContextProperty("appColors", settings_manager.GetColors());
+  engine.rootContext()->setContextProperty("appColorModes", settings_manager.GetColorModes());
+  engine.rootContext()->setContextProperty("appCurrentColorMode", settings_manager.GetCurrentColorMode());
 
   const QUrl url(QStringLiteral("qrc:/main.qml"));
   QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
