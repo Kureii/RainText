abe84a1515187f495c425888c7e33bdb40d4d9ec
Tomas Adamek (Kureii) <ad.tomik@seznam.cz>
2024-02-12 00:39:03 +0100

    add czech translate

---
+++ locales/czech-cz_cs.ts
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>LoginRegisterScreen</name>
+    <message>
+        <location filename="../qml/LoginRegisterScreen.qml" line="77"/>
+        <location filename="../qml/LoginRegisterScreen.qml" line="96"/>
+        <source>Register</source>
+        <translation>Registrovat</translation>
+    </message>
+    <message>
+        <location filename="../qml/LoginRegisterScreen.qml" line="119"/>
+        <location filename="../qml/LoginRegisterScreen.qml" line="138"/>
+        <location filename="../qml/LoginRegisterScreen.qml" line="211"/>
+        <location filename="../qml/LoginRegisterScreen.qml" line="328"/>
+        <source>Login</source>
+        <translation>Přihlásit se</translation>
+    </message>
+    <message>
+        <location filename="../qml/LoginRegisterScreen.qml" line="223"/>
+        <source>Username</source>
+        <translation>Uživatelské jméno</translation>
+    </message>
+    <message>
+        <location filename="../qml/LoginRegisterScreen.qml" line="248"/>
+        <source>Password</source>
+        <translation>Heslo</translation>
+    </message>
+    <message>
+        <location filename="../qml/LoginRegisterScreen.qml" line="302"/>
+        <source>Password again</source>
+        <translation>Heslo znovu</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordManagerScreen</name>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="69"/>
+        <source>Add record</source>
+        <translation>Přidat záznam</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="89"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="141"/>
+        <source>Create record</source>
+        <translation>Vytvořit záznam</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="157"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="534"/>
+        <source>Headline</source>
+        <translation>Nadpis</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="179"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="556"/>
+        <source>Username</source>
+        <translation>Uživatelské jméno</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="202"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="612"/>
+        <source>Password</source>
+        <translation>Heslo</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="245"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="655"/>
+        <source>Upper letters</source>
+        <translation>Velká písmena</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="262"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="672"/>
+        <source>Lower letters</source>
+        <translation>Malá písmena</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="288"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="697"/>
+        <source>Numbers</source>
+        <translation>Číslice</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="305"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="714"/>
+        <source>Special chars</source>
+        <translation>Speciální znaky</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="337"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="746"/>
+        <source>Password length</source>
+        <translation>Délka hesla</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="382"/>
+        <location filename="../qml/PasswordManagerScreen.qml" line="791"/>
+        <source>Generate password</source>
+        <translation>Generovat heslo</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="433"/>
+        <source>Save record</source>
+        <translation>Uložit záznam</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="518"/>
+        <source>Edit record</source>
+        <translation>Upravit záznam</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="585"/>
+        <source>Old password</source>
+        <translation>Staré heslo</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="845"/>
+        <source>Save changes</source>
+        <translation>Uložit změny</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordManagerScreen.qml" line="960"/>
+        <source>Encrypting iterations</source>
+        <translation>Iterace šifrování</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordRecordListItem</name>
+    <message>
+        <location filename="../qml/PasswordRecordListItem.qml" line="214"/>
+        <source>Headline was copied to clipboard successfully</source>
+        <translation>Nadpis byl úspěšně zkopírován do schránky</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordRecordListItem.qml" line="262"/>
+        <source>Username was copied to clipboard successfully</source>
+        <translation>Uživatelské jméno bylo úspěšně zkopírováno do schránky</translation>
+    </message>
+    <message>
+        <location filename="../qml/PasswordRecordListItem.qml" line="342"/>
+        <source>Password was copied to clipboard successfully</source>
+        <translation>Heslo bylo úspěšně zkopírováno do schránky</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/gui/login_register_manager.cpp" line="86"/>
+        <location filename="../src/gui/login_register_manager.cpp" line="250"/>
+        <source>Generating key</source>
+        <translation>Generovat klíč</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/login_register_manager.cpp" line="172"/>
+        <source>Done</source>
+        <translation>Hotovo</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/login_register_manager.cpp" line="308"/>
+        <source>decrypted</source>
+        <translation>dešifrováno</translation>
+    </message>
+    <message>
+        <location filename="../src/setting_loader.cpp" line="71"/>
+        <location filename="../src/setting_loader.cpp" line="85"/>
+        <location filename="../src/setting_loader.cpp" line="145"/>
+        <source>Settings file</source>
+        <translation>Soubor nastavení</translation>
+    </message>
+    <message>
+        <location filename="../src/setting_loader.cpp" line="72"/>
+        <source>The settings file cannot be opened.</source>
+        <translation>Soubor s nastavení se nepovedlo otevřít.</translation>
+    </message>
+    <message>
+        <location filename="../src/setting_loader.cpp" line="86"/>
+        <source>The settings file contains an unexpected structure.</source>
+        <translation>Soubor nastavení obsahuje neočekávanou strukturu.</translation>
+    </message>
+    <message>
+        <location filename="../src/setting_loader.cpp" line="146"/>
+        <source>Invalid color</source>
+        <translation>Neplatná barva</translation>
+    </message>
+</context>
+</TS>
