e048a70957cbb27c19e95150e77f9b605feab4f3
Tomas Adamek (Kureii) <ad.tomik@seznam.cz>
2024-02-12 02:50:45 +0100

    Clear project

--- src/headers/pass_gen.h
+++
@@ -1,9 +0,0 @@
-#ifndef RAINTEXT_PASS_GEN_H
-#define RAINTEXT_PASS_GEN_H
-
-#include <iostream>
-namespace rain_text::pass_gen {
-std::string GeneratePassword(uint8_t len = 16);
-}
-
-#endif  // RAINTEXT_PASS_GEN_H
